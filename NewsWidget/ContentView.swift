d5d1ade80fd2008f0a7074059c18529b196a5012
guan-kevin <github@pigzy.net>
2021-07-04 17:03:20 -0700

    Initial Commit

---
+++ NewsWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsWidget
+//
+//  Created by Kevin Guan on 7/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsWidget/NewsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWidgetApp.swift
+//  NewsWidget
+//
+//  Created by Kevin Guan on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
